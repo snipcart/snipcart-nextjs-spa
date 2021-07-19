21bd025f5c7c1cde631c2e29b85a9a7ed11ddbab
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2021-07-18 20:49:27 -0400

    Use product image src in API

--- pages/api/products/[productId].ts
+++ pages/api/products/[productId].ts
@@ -4,9 +4,23 @@ import {NextApiRequest, NextApiResponse} from "next";
 import {products} from "../../index";
 import {IProduct} from "../../../components/Product";
 
+export interface ISnipcartProduct {
+    id: string
+    name: string
+    price: number
+    url: string
+    description: string
+    image: string
+}
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
     const {productId} = req.query;
     const product: IProduct | undefined = products.find(p => p.id === productId);
+    if (!product) {
+        res.status(404);
+        return ;
+    }
+    const snipcartProduct: ISnipcartProduct = {...product, image: product?.image.src ?? ""}
 
-    res.status(200).json(product);
+    res.status(200).json(snipcartProduct);
 }
